45b77f314599c3eb5317b11c42a4bb90060e44a3
mayurmb.mb@gmail.com <66797506+mayurbagewadi@users.noreply.github.com>
2026-06-05 23:27:24 +0530

    Remove storefront runtime debug check

--- src/storefront-main.tsx
+++ src/storefront-main.tsx
@@ -1,8 +1,5 @@
 import { createRoot } from "react-dom/client";
 import StorefrontApp from "./StorefrontApp.tsx";
 import "./index.css";
-import { installStorefrontDebug } from "@/lib/storefrontDebug";
-
-installStorefrontDebug();
 
 createRoot(document.getElementById("root")!).render(<StorefrontApp />);
