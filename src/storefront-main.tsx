5bd53647a7fbdf66495a9d7741ba010f2d5e605c
mayurmb.mb@gmail.com <66797506+mayurbagewadi@users.noreply.github.com>
2026-06-05 23:06:32 +0530

    Add storefront runtime debug check

--- src/storefront-main.tsx
+++ src/storefront-main.tsx
@@ -1,5 +1,8 @@
 import { createRoot } from "react-dom/client";
 import StorefrontApp from "./StorefrontApp.tsx";
 import "./index.css";
+import { installStorefrontDebug } from "@/lib/storefrontDebug";
+
+installStorefrontDebug();
 
 createRoot(document.getElementById("root")!).render(<StorefrontApp />);
